719c8098477c0ae3e3367324665bc4b1dfd9394e
Payton Banks <paytonbanks@Mac-Payton.local>
2020-10-20 22:57:54 -0400

    updated core React files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect } from 'react';
+
+
+
+export function App() {
+  const [data, setData] = useState([])
+
+  
+  return (
+    <div>Employee Table</div>
+  );
+}
+
+export default App;
